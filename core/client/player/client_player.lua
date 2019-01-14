f259bff45cac2e6f68e9940ae42a4d23acdb9f33
Loqrin <loqrin@gmail.com>
2019-01-14 12:24:44 +0200

    Fixed invincibility... pls?

--- core/client/player/client_player.lua
+++ core/client/player/client_player.lua
@@ -161,8 +161,9 @@ Citizen.CreateThread(function()
     while true do 
         local plyPed = GetPlayerPed(-1)
 
-        --SetEntityInvincible(currentVehicle, enableVehicleInvincibility)
-        --SetEntityInvincible(plyPed, enablePlyInvincibility)
+        SetEntityInvincible(currentVehicle, false)
+        SetEntityInvincible(plyPed, false)
+        SetPlayerInvincible(plyPed, false)
 
         Citizen.Wait(1)
     end
